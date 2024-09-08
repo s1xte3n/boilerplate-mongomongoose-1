5a3b27aff77ba1c6cb4f0281a0aed6fe4f2d464d
Ntlantla Thokozani Nhlapo <ytp2580@gmail.com>
2024-09-08 23:41:08 +0000

    1) Install & Set up mongoose

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 /* 1) Install & Set up mongoose */
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 let Person;
 
